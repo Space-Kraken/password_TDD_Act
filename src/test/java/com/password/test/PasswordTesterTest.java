9db1834031bf63630f5c33482fca810a212c53a6
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2020-10-27 21:10:20 -0600

    Modificacion_pruebas_unitarias
    Modificación de las pruebas untarias para comprobar

--- src/test/java/com/password/test/PasswordTesterTest.java
+++ src/test/java/com/password/test/PasswordTesterTest.java
@@ -9,18 +9,18 @@ public class PasswordTesterTest {
 
     @Test
     public void weak_when_has_less_than_8_letters(){
-        assertEquals(WEAK, PasswordTester.assesPassword("123asd!"));
+        assertEquals(WEAK, PasswordTester.assesPassword("p0ll1to"));
     }
     @Test
     public void weak_when_has_only_letters(){
-        assertEquals(WEAK, PasswordTester.assesPassword("abcdefgh"));
+        assertEquals(WEAK, PasswordTester.assesPassword("Lavacamu"));
     }
     @Test
     public void medium_when_has_letters_and_numbers(){
-        assertEquals(MEDIUM, PasswordTester.assesPassword("abcd1234"));
+        assertEquals(MEDIUM, PasswordTester.assesPassword("shrek12345"));
     }
     @Test
     public void strong_when_has_letters_numbers_and_symbols(){
-        assertEquals(STRONG, PasswordTester.assesPassword("abcd123!"));
+        assertEquals(STRONG, PasswordTester.assesPassword("conejomalo+"));
     }
 }
