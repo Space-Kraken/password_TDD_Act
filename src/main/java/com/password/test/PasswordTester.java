9484521a2a6d57edcb357a5a68e2eec2df737da8
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2020-10-27 22:09:49 -0600

    Interfaz
    Se agrego una ventana para ingresar la contraseña que se evaluará

--- src/main/java/com/password/test/PasswordTester.java
+++ src/main/java/com/password/test/PasswordTester.java
@@ -1,9 +1,33 @@
 package com.password.test;
 
+import javax.swing.*;
+
 public class PasswordTester {
     public enum SecurityLevel{
         WEAK, MEDIUM, STRONG
     }
+
+    public static void main(String[]args) {
+        int condicion = 0;
+        while (condicion != 1) {
+            String contra = JOptionPane.showInputDialog("Password: ");
+            JOptionPane.showMessageDialog(null, "Password Status: " + assesPassword(contra));
+
+             condicion = JOptionPane.showOptionDialog(
+                    null,
+                    "Choose an option",
+                    "Selector de opciones",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    new Object[] { "Enter new password", "Exit" },   // null para YES, NO y CANCEL
+                    "opcion 1");
+
+
+        }
+
+    }
+
     public static SecurityLevel assesPassword(String password){
 
         if (password.length()<8){
